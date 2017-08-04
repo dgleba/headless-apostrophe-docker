d49dbe8389f91ca35ec4b075900fd183604d2596
Anthony Tarlao <anthony.tarlao@gmail.com>
2017-08-04 09:50:09 +0200

    clean config js

--- config/default-docker.js
+++ config/default-docker.js
@@ -1,8 +1,5 @@
 module.exports = {
   mongo: {
-    uri:
-      process.env.MONGODB ||
-      `mongodb://${process.env.MONGODB_PORT_27017_TCP_ADDR}:${process.env
-        .MONGODB_PORT_27017_TCP_PORT}/site`
+    uri: process.env.MONGODB || `mongodb://${process.env.MONGODB_PORT_27017_TCP_ADDR}:${process.env.MONGODB_PORT_27017_TCP_PORT}/site`
   }
 }
