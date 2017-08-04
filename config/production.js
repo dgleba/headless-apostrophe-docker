e68b4f804c2ce346f635c77f44b7a70245eca450
Anthony Tarlao <anthony.tarlao@gmail.com>
2017-08-04 09:46:08 +0200

    remove style-loader, docker scripts in npm

--- config/production.js
+++ config/production.js
@@ -1,8 +1,1 @@
-module.exports = {
-  mongo: {
-    uri:
-      process.env.MONGODB ||
-      `mongodb://${process.env.MONGODB_PORT_27017_TCP_ADDR}:${process.env
-        .MONGODB_PORT_27017_TCP_PORT}/site`
-  }
-}
+module.exports = {}
